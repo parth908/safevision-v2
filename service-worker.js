1aeb32c13aa23c298ce9e1392f8cb10e4f2d3844
parth908 <parthvats2342@gmail.com>
2025-07-18 13:12:09 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,65 +1,21 @@
-const CACHE_NAME = 'safevision-v2-cache-v3'; // Changed to v3 as requested
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png', 
-  './icon-512.png', 
-  './call-112-screenshot.png' 
-  // Note: ResponsiveVoice.js is loaded from a CDN and won't be cached by this service worker.
-];
-
-self.addEventListener('install', (event) => {
-  console.log('[Service Worker] Installing - v3. Taking control immediately.'); 
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('[Service Worker] Opened cache, adding core URLs.');
-        return cache.addAll(urlsToCache);
-      })
-      .then(() => self.skipWaiting())
-      .catch(err => console.error('[Service Worker] Cache addAll failed:', err))
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('[Service Worker] Activating - v3. Claiming clients.'); 
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            console.log(`[Service Worker] Deleting old cache: ${cacheName}`);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-    .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request).then(
-          function(response) {
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            var responseToCache = response.clone();
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-            return response;
-          }
-        );
-      })
-  );
-});
+{
+  "name": "SafeVision V2 App",
+  "short_name": "SafeVisionV2",
+  "description": "An enhanced assistive app with emergency call simulation.",
+  "start_url": "./",
+  "display": "fullscreen",
+  "background_color": "#000000",
+  "theme_color": "#000000",
+  "icons": [
+    {
+      "src": "icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
